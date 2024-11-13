7a8e458f43d3839bb4c161146c66e84598a50808
lallox <dliaadam527@gmail.com>
2024-11-13 10:26:54 +0100

    Add files via upload

---
+++ exercice algo python/exercice_py_algo.py
@@ -0,0 +1,25 @@
+#exercice 1
+'''i = 1
+while i < 11:
+  print("hello world")
+  i += 1 '''
+#exercice 2
+'''somme = 0
+for i in range (1,51) :
+    somme +=i
+print("la somme des 50 premier entiers est : ", somme)'''
+#exercice 3 
+'''n = int(input("Entrez un entier : "))
+somme = 0
+for i in range(1, n + 1):
+    somme += i
+print("La somme des entiers de 1 à n est :", somme)
+'''
+#exercice 4
+'''n = int(input("Entrez un nombre  : "))
+factoriel = 1
+for i in range(1, n + 1):
+    factoriel *= i
+print("Le factoriel de n est :" , factoriel)
+'''
+#exercice 5
